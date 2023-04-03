002a1b05f6b5b9ed2ed0887292ddb6fc129682ee
Chen Jiang <chen.jiang.1206@gmail.com>
2023-04-03 17:46:56 -0400

    add onchange event

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const localHouseUrl = "http://localhost:3000/houses"
 const houseBar = document.getElementById('house-bar')
 const houseBasic = document.getElementById('house-basic-info')
+const dropdownBox = document.getElementById('traits-dropdown')
+const sortingAnnouncement = document.getElementById('sorted-announcement')
 
 function fetchLocalHouses() {
     fetch(localHouseUrl)
@@ -28,4 +30,51 @@ function displayCrestAndInfo(event, house) {
     
     const houseAnimal = document.getElementById('house-animal')
     houseAnimal.textContent = house.animal
+}
+
+dropdownBox.onchange = (event) => displayChosenHouse(event)
+
+function displayChosenHouse(event) {
+    const option = event.target.value 
+
+    if (option === 'a' || option === "h" ) {
+        console.log('Gryffindor')
+        // fetchChosenSchool(1)
+        announceHouseAndPassword('Gryffindor')
+        // THEN HOUSE1 
+        // 1) ANNOUNCING THE CHOSEN HOUSE + PASSWORD 
+        // 3) HIDE THE DROPDOWN BOX 
+    
+    } else if (option === "c" || option === "e") {
+        console.log('Slytherin')
+        // THEN HOUSE2 
+        // 1) ANNOUNCING THE CHOSEN HOUSE + PASSWORD 
+        // 3) HIDE THE DROPDOWN BOX 
+    } else if (option === "b" || option === "d") {
+        console.log('Hufflepuff')
+
+    } else {
+        console.log('Ravenclaw')
+    }
+
+    //hiding the house bar 
+    houseBar.style.display = "none"
+
+}
+
+function fetchChosenSchool(id) {
+    fetch(localHouseUrl)
+    .then(r => r.json())
+    .then(data => data.forEach(house => )) 
+}
+    
+function announceHouseAndPassword(houseName) {
+    const announcement = document.getElementById('sorted-announcement')
+
+    const message = document.createElement('p')
+    message.textContent = `${houseName}!!!`
+
+    announcement.appendChild(message)
+    
+
 }
