daf5d172cad4ebc4eb52e943ddd790346d2f989c
Chen Jiang <chen.jiang.1206@gmail.com>
2023-04-03 20:55:20 -0400

    add fetch API event listener

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const localHouseUrl = "http://localhost:3000/houses/"
+const wizardWorldUrl = "https://wizard-world-api.herokuapp.com/Houses/"
+const hpApiUrl = "https://hp-api.onrender.com/api/characters/house/"
+
 const houseBar = document.getElementById('house-bar')
 const houseBasic = document.getElementById('house-basic-info')
 const dropdownBox = document.getElementById('traits-dropdown')
 const sortingAnnouncement = document.getElementById('sorted-announcement')
 const passwordForm = document.getElementById('enter-password-form')
+
+
+const idGryffindor = "0367baf3-1cb6-4baf-bede-48e17e1cd005/"
+const idRavenclaw = "805fd37a-65ae-4fe5-b336-d767b8b7c73a/"
+const idHufflepuff = "85af6295-fd01-4170-a10b-963dd51dce14/"
+const idSlytherin = "a9704c47-f92e-40a4-8771-ed1899c9b9c1/"
 
 function fetchLocalHouses() {
     fetch(localHouseUrl)
@@ -47,7 +56,7 @@ function displayChosenHouse(event) {
     }
 
     houseBar.style.display = "none"
-    document.getElementById('sorting-question').style.display = "none"
+    // document.getElementById('sorting-question').style.display = "none"
 }
 
 function fetchChosenSchool(id) {
@@ -75,4 +84,42 @@ function announceHouseAndPassword(house) {
     message.appendChild(password)
 
     passwordForm.style.display = "block"
+    passwordForm.onsubmit = (event) => fetchApiInfo(event, house)
+}
+
+function fetchApiInfo(event, house) {
+    event.preventDefault()
+    let wizardWorldHouseUrl = ""
+    let characterUrl = ""
+//check password 
+    if (house.id === 1) {
+        wizardWorldHouseUrl = wizardWorldUrl + idGryffindor
+        characterUrl = hpApiUrl + "Gryffindor"
+    } else if (house.id === 2) {
+        wizardWorldHouseUrl = wizardWorldUrl + idSlytherin
+        characterUrl = hpApiUrl + "Slytherin"
+    } else if (house.id === 3) {
+        wizardWorldHouseUrl = wizardWorldUrl + idHufflepuff
+        characterUrl = hpApiUrl + "Hufflepuff"
+    } else {
+        wizardWorldHouseUrl = wizardWorldUrl + idRavenclaw
+        characterUrl = hpApiUrl + "Ravenclaw"
+    }
+
+    fetch(wizardWorldHouseUrl)
+    .then(r => r.json())
+    .then(houseData => renderWizardWorldInfo(houseData))
+
+    fetch(characterUrl)
+    .then(r => r.json())
+    .then(characterData => characterData.forEach( character => renderCharacterInfo(character)))
+
+}
+
+function renderWizardWorldInfo(house) {
+    console.log(house)
+}
+
+function renderCharacterInfo(character) {
+    console.log(character)
 }
